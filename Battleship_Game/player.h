6ddd6e2fd58a7de27f2d26ddb4bcccda39a82ebb
A0L0XIIV <brnky_95@hotmail.com>
2020-02-08 21:00:57 -0500

    Place ship func. initial commit

--- Battleship_Game/player.h
+++ Battleship_Game/player.h
@@ -2,6 +2,10 @@
 #define horizantalSize 10
 #define verticalSize 10
 
+#include <iostream>;
+
+using namespace std;
+
 class player {
 private:
 	// Grid areas - Ships represented as C1,C2,C3,... and D1,D2,D3
@@ -20,12 +24,64 @@ public:
 	// Constructer
 	player();
 	// Functions
+	void placeShips();
 	bool enemyAttack(int x, int y);
 	bool didShipSink(bool *ship);
 	// Destroyer
 	~player();
 
 };
+
+// Start - Place ships
+void player::placeShips() {
+
+	const char* shipNames[5] = { "Carrier", "Battleship", "Destroyer", "Submarine", "Corvette" };
+	const int shipLengths[5] = { 5, 4, 4, 3, 2 };
+
+	// Loop for all 5 ship types
+	for (int i = 0; i < 5; i++) {
+		int x_input = 0;
+		int y_input = 0;
+		char direction = NULL;
+
+		// Get input values from user
+		cout << shipNames[i] << " ship placement: 5 blocks" << endl;
+		cout << shipNames[i] << " x-axis (0-9):";
+		cin >> x_input;
+		cout << shipNames[i] << " y-axis (0-9):";
+		cin >> y_input;
+		cout << shipNames[i] << " direction (U, D, L, R):";
+		cin >> direction;
+
+		// Check area constraints - Out of area
+		if (direction == 'U' || direction == 'u') {
+			if (y_input - (shipLengths[i]-1) < 0) {
+				//BREAK OUT OF AREA
+			}
+		}
+		else if (direction == 'D' || direction == 'd') {
+			if (y_input + (shipLengths[i] - 1) > 9) {
+				//BREAK OUT OF AREA
+			}
+		}
+		else if (direction == 'L' || direction == 'l') {
+			if (x_input - (shipLengths[i] - 1) < 0) {
+				//BREAK OUT OF AREA
+			}
+		}
+		else if (direction == 'R' || direction == 'r') {
+			if (x_input + (shipLengths[i] - 1) > 9) {
+				//BREAK OUT OF AREA
+			}
+		}
+
+		// Place ship's initial block in area
+		this->playerArea[x_input][y_input] = shipNames[i][0] + '1'; // C1, B1, ...
+		// Loop over other blocks with direction
+		// TO-DO
+
+	}
+}
 
 // Gets coordinates as a input, returns true on hit - false on miss
 bool player::enemyAttack(int x, int y) {
